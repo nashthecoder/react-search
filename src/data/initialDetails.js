c80953218ba6f95f59a359bc93afbcbd679d0627
nashthecoder <naijeria@gmail.com>
2022-09-11 13:09:55 +0300

    created simple react search

---
+++ src/data/initialDetails.js
@@ -0,0 +1,39 @@
+const initialDetails = [
+    {
+        id: 1,
+        imgPath: "/assets/img/1.png",
+        name: "Jane Doe",
+        email: "janedoe@gmail.com",
+        address: "Nairobi, Kenya",
+    },
+    {
+        id: 2,
+        imgPath: "/assets/img/2.png",
+        name: "Mary Rosamund",
+        email: "maryrose@rosie.com",
+        address: "Mombasa, Kenya",
+    },
+    {
+        id: 3,
+        imgPath: "/assets/img/3.png",
+        name: "Sherly Watson",
+        email: "irene@johnlock.com",
+        address: "Kisumu, Kenya",
+    },
+    {
+        id: 4,
+        imgPath: "/assets/img/4.png",
+        name: "Judy Holmes",
+        email: "judy@johnlock.com",
+        address: "Eldoret, Kenya",
+    },
+    {
+        id: 5,
+        imgPath: "/assets/img/5.png",
+        name: "Myra Lestrade",
+        email: "britishgovt@gmail.com",
+        address: "Nairobi, Kenya",
+    },
+];
+
+export default initialDetails;
